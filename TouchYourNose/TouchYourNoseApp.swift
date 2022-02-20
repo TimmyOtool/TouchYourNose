62a98e398790bf5af0052dcc9a8ad0d601cc27ed
Michaël Melin <michael.melin@etnic.be>
2022-02-20 17:40:56 +0100

    Initial Commit

---
+++ TouchYourNose/TouchYourNoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchYourNoseApp.swift
+//  TouchYourNose
+//
+//  Created by Michael Melin on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchYourNoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
