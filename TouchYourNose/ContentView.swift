d99762e107d95f6096d6374ceb25f02bd8446cf9
Michaël Melin <michael.melin@etnic.be>
2022-02-20 18:33:41 +0100

    feat: add notification scheduler

--- TouchYourNose/ContentView.swift
+++ TouchYourNose/ContentView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }.frame(width: 300, height: 50, alignment: .center).font(.system(size: 33))
+
+            
+ 
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Touch Your Nose"
+                
+                content.sound = UNNotificationSound.default
+
+                for i in 0...24 {
+     
+                // Setup trigger time
+                var date = DateComponents()
+                date.hour = i
+                date.minute = i
+                    
+                    content.subtitle = "It's "+date.hour+":"+date.minute
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+
+                // Create request
+                let uniqueID = UUID().uuidString // Keep a record of this if necessary
+                let request = UNNotificationRequest(identifier: uniqueID, content: content, trigger: trigger)
+    
+    
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)
+                }
+            }.frame(width: 300, height: 50, alignment: .center).font(.system(size: 33))
+        }
     }
 }
 
